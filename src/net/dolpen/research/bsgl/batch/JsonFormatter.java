254084801720dbc35a877b3c57230458ffc84604
dolpen <dolpen@dolpen.net>
2014-10-22 16:29:46 +0900

    jsonFormatterが必要なディレクトリ以下全てのファイルをフォーマットするように変更

--- src/net/dolpen/research/bsgl/batch/JsonFormatter.java
+++ src/net/dolpen/research/bsgl/batch/JsonFormatter.java
@@ -1,21 +1,41 @@
 package net.dolpen.research.bsgl.batch;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonParser;
+import com.google.gson.*;
 import net.dolpen.research.bsgl.api.Cache;
+import net.dolpen.research.bsgl.util.Const;
+
+import java.io.File;
+import java.util.*;
 
 /**
  * Format Large Json (ex. master/api_start2)
  */
 public class JsonFormatter {
+
+    static final String dirName = "/inputs";
+
     public static void main(String... args) throws Exception {
-        final String path = "/inputs/master/start2_master.txt";
-        String master = Cache.load(path);
+        List<String> names = getNames(Const.ROOT, dirName);
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
         JsonParser jp = new JsonParser();
-        JsonElement je = jp.parse(master);
-        Cache.save(path, gson.toJson(je));
+        for (String name : names) {
+            String data = Cache.load(name);
+            JsonElement je = jp.parse(data);
+            Cache.save(name, gson.toJson(je));
+        }
+    }
+
+    public static List<String> getNames(String prefix, String path) {
+        List<String> ret = new ArrayList<String>();
+        File f = new File(prefix + path);
+        if (!f.exists()) return ret;
+        if (f.isFile()) {
+            ret.add(path);
+        } else if (f.isDirectory()) {
+            for (String s : f.list()) {
+                ret.addAll(getNames(prefix, path + "/" + s));
+            }
+        }
+        return ret;
     }
 }
